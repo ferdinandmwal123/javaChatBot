3a88dafc81ac8021a4782f2b270e49f5bc9808fa
Your Name <ferdinandmwal@gmail.com>
2019-09-28 15:57:39 +0300

    Completed

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -48,12 +48,64 @@ public class Bot extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent keyEvent) {
           if(keyEvent.getKeyCode() ==KeyEvent.VK_ENTER){//if enter is pressed
-              input.setEditable(false);//dont recognise as
+              input.setEditable(false);//don't recognise as go down a line
+              //---------get input-------------
+              String userInput = input.getText();
+              input.setText("");//clear text
+              addText("-->You:\t" + userInput);
+              userInput.trim();
+              while (
+                      userInput.charAt(userInput.length()-1)== '!' ||
+                              userInput.charAt(userInput.length()-1)== '.' ||
+                              userInput.charAt(userInput.length()-1)== '?'
+              ){
+                  userInput=userInput.substring(0,userInput.length()-1); //gets beginning character to last character
+
+              }
+              userInput.trim();
+              byte response = 0; //0 means searching in bot array
+                                 //1 nothing found
+                                 //2 found
+
+              //---------check for matches---------
+              int j = 0; //which group in array we are checking (greetings, question)
+              while (response == 0){
+                  if (inArray(userInput.toLowerCase(), bot[j * 2])); //since only 2 groups exist greetings 0 *2 & questions 1 * 2
+                  response = 2;
+                  int r= (int) Math.floor(Math.random() *bot[(j * 2) + 1].length);
+                  addText("\n-->Bot" + bot[(j*2)][r]);
+              }
+               j++;
+              if(j*2==bot.length-1 && response==0){
+                  response=1;
+              }
+
+              //---------else default----
+              if(response==1){
+                  int r= (int) Math.floor(Math.random() *bot[bot.length-1].length);
+                  addText("\n-->Bot" + bot[bot.length-1][r]);
+              }
+              addText("\n");
           }
     }
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
+        if(keyEvent.getKeyCode() ==KeyEvent.VK_ENTER){//if enter is pressed
+            input.setEditable(true);//don't recognise as go down a line
+        }
+    }
 
+    public void addText(String str){
+      dialog.setText(dialog.getText() + str);
+    }
+    public boolean inArray(String in, String[] str){
+      boolean match = false;
+     for(int i = 0; i < str.length; i++){
+         if(str[i].equals(in)){
+             match = true;
+         }
+     }
+     return match;
     }
 }
